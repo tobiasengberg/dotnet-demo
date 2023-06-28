7b4af04d05590a50d291aba1367b26d9d3b64e3e
Exilic <tobias.engberg@exilic.se>
2023-06-28 09:58:37 +0200

    Added formatting methods to Reference class

--- Domain/Reference.cs
+++ Domain/Reference.cs
@@ -4,4 +4,19 @@ public class Reference
 {
     public int ID { get; set; }
     public Source Source { get; set; }
+
+    public String FormattedAsAPA()
+    {
+        return "";
+    }
+    
+    public String FormattedAsHarvard()
+    {
+        return "";
+    }
+    
+    public String FormattedAsOxford()
+    {
+        return "";
+    }
 }
